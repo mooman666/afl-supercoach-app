7ab8926f618e7ad3722f0be758f6319735d7b4fe
mooman666 <ramartin@post.com>
2026-04-24 04:35:16 +1000

    Update players.js

--- players.js
+++ players.js
@@ -1,37 +1,38 @@
 const players = [
   {
     name: "Lachie Neale",
-    team: "BRL",
     position: "MID",
     price: 680000,
     scores: [118, 123, 120, 110, 125]
   },
   {
     name: "Patrick Cripps",
-    team: "CAR",
     position: "MID",
     price: 650000,
     scores: [110, 117, 118, 105, 122]
   },
   {
-    name: "Max Gawn",
-    team: "MEL",
-    position: "RUC",
-    price: 610000,
-    scores: [105, 112, 107, 115, 109]
-  },
-  {
     name: "Marcus Bontempelli",
-    team: "WBD",
     position: "MID/FWD",
     price: 670000,
     scores: [116, 119, 120, 113, 121]
   },
   {
     name: "Jordan Dawson",
-    team: "ADEL",
     position: "DEF",
     price: 590000,
     scores: [107, 110, 111, 109, 114]
+  },
+  {
+    name: "Max Gawn",
+    position: "RUC",
+    price: 610000,
+    scores: [105, 112, 107, 115, 109]
+  },
+  {
+    name: "Caleb Serong",
+    position: "MID",
+    price: 600000,
+    scores: [108, 111, 112, 110, 114]
   }
 ];
