5910296c09d7ff448e8c22ecc52c3ca82d9d1ab6
mooman666 <ramartin@post.com>
2026-04-24 02:01:30 +1000

    Create players.js

---
+++ players.js
@@ -0,0 +1,7 @@
+const players = {
+  neale: { name:"Lachie Neale", pos:"MID", price:680000, avg:121, scores:[118,123,120] },
+  cripps: { name:"Patrick Cripps", pos:"MID", price:650000, avg:115, scores:[110,117,118] },
+  gawn: { name:"Max Gawn", pos:"RUC", price:610000, avg:108, scores:[105,112,107] },
+  bont: { name:"Marcus Bontempelli", pos:"MID/FWD", price:670000, avg:118, scores:[116,119,120] },
+  dawson: { name:"Jordan Dawson", pos:"DEF", price:590000, avg:109, scores:[107,110,111] }
+};
