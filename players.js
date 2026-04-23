08f391bd6af2a0adeb0248cb8be049419c68eb5b
mooman666 <ramartin@post.com>
2026-04-24 02:16:44 +1000

    Update players.js

--- players.js
+++ players.js
@@ -3,5 +3,7 @@ const players = {
   cripps: { name:"Patrick Cripps", pos:"MID", price:650000, avg:115, scores:[110,117,118] },
   gawn: { name:"Max Gawn", pos:"RUC", price:610000, avg:108, scores:[105,112,107] },
   bont: { name:"Marcus Bontempelli", pos:"MID/FWD", price:670000, avg:118, scores:[116,119,120] },
-  dawson: { name:"Jordan Dawson", pos:"DEF", price:590000, avg:109, scores:[107,110,111] }
+  dawson: { name:"Jordan Dawson", pos:"DEF", price:590000, avg:109, scores:[107,110,111] },
+  serong: { name:"Caleb Serong", pos:"MID", price:600000, avg:110, scores:[108,111,112] },
+  rowell: { name:"Matt Rowell", pos:"MID", price:540000, avg:106, scores:[104,108,106] }
 };
