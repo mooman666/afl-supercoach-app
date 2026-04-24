1bea1bda34f3a7fe06462612e0e92045f0cc73f2
mooman666 <ramartin@post.com>
2026-04-24 18:46:55 +1000

    Update players.js

--- players.js
+++ players.js
@@ -1,6 +1,11 @@
 const playerDatabase = [
     { name: "Angus Anderson", team: "Magpies", pos: "MID", price: 113500, be: -75, status: "IN", own: "45%" },
     { name: "Harry Sheezel", team: "Kangaroos", pos: "DEF", price: 645000, be: 112, status: "TEST", own: "70%" },
-    { name: "Nick Daicos", team: "Magpies", pos: "MID", price: 636200, be: 129, status: "IN", own: "35%" },
     { name: "Isaac Heeney", team: "Swans", pos: "MID", price: 632700, be: 24, status: "OUT", own: "10%" }
+];
+
+const injuries = [
+    { name: "Isaac Heeney", team: "Swans", injury: "Calf", return: "R7 OUT" },
+    { name: "Harry Sheezel", team: "Kangaroos", injury: "Finger", return: "TEST" },
+    { name: "Jarrod Berry", team: "Lions", injury: "Calf", return: "3-4 Weeks" }
 ];
