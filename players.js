f46b5dcfe361c80b7011e02e3b5b4f09dde06be9
mooman666 <ramartin@post.com>
2026-04-24 15:09:04 +1000

    Update players.js

--- players.js
+++ players.js
@@ -1,9 +1,12 @@
 const playerDatabase = [
-    { name: "Max Gawn", team: "Demons", pos: "RUC", price: 650000, avg: 134.2 },
-    { name: "Brodie Grundy", team: "Swans", pos: "RUC", price: 645000, avg: 133.0 },
-    { name: "Nick Daicos", team: "Magpies", pos: "MID", price: 710000, avg: 125.4 },
-    { name: "Harry Sheezel", team: "Kangaroos", pos: "DEF", price: 680000, avg: 128.0 },
-    { name: "Jordan Dawson", team: "Crows", pos: "MID", price: 620000, avg: 110.2 },
-    { name: "Isaac Heeney", team: "Swans", pos: "FWD", price: 590000, avg: 128.0 },
-    { name: "Hugh Bond", team: "Crows", pos: "DEF", price: 167800, avg: 83.0 }
+    { name: "Max Gawn", team: "Demons", pos: "RUC", price: 650000, avg: 134.2, bye: 14 },
+    { name: "Brodie Grundy", team: "Swans", pos: "RUC", price: 645000, avg: 133.0, bye: 12 },
+    { name: "Nick Daicos", team: "Magpies", pos: "MID", price: 710000, avg: 125.4, bye: 15 },
+    { name: "Harry Sheezel", team: "Kangaroos", pos: "DEF", price: 680000, avg: 128.0, bye: 14 },
+    { name: "Jordan Dawson", team: "Crows", pos: "MID", price: 620000, avg: 110.2, bye: 14 },
+    { name: "Isaac Heeney", team: "Swans", pos: "FWD", price: 590000, avg: 128.0, bye: 12 },
+    { name: "Hugh Bond", team: "Crows", pos: "DEF", price: 167800, avg: 83.0, bye: 14 },
+    { name: "Sam Flanders", team: "Suns", pos: "FWD", price: 580000, avg: 115.5, bye: 12 },
+    { name: "Tom Stewart", team: "Cats", pos: "DEF", price: 630000, avg: 108.9, bye: 15 },
+    { name: "Caleb Serong", team: "Dockers", pos: "MID", price: 690000, avg: 122.1, bye: 13 }
 ];
