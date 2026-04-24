f03f2a065f74fc1d37d366fff720a1e60f7ba9f5
mooman666 <ramartin@post.com>
2026-04-24 18:24:08 +1000

    Update players.js

--- players.js
+++ players.js
@@ -1,16 +1,16 @@
 const playerDatabase = [
-    { name: "Angus Anderson", team: "Magpies", pos: "MID", price: 113500, be: -33, status: "IN", own: "15.7%", vestRisk: "LOW" },
-    { name: "Nasiah Wanganeen-Milera", team: "Saints", pos: "DEF", price: 576600, be: 108, status: "IN", own: "31.4%", vestRisk: "ZERO" },
-    { name: "Harry Sheezel", team: "Kangaroos", pos: "DEF", price: 611700, be: 140, status: "IN", own: "99.7%", vestRisk: "ZERO" },
-    { name: "Isaac Heeney", team: "Swans", pos: "MID", price: 632700, be: 24, status: "OUT", own: "10.6%", vestRisk: "HIGH" },
-    { name: "Elijah Tsatas", team: "Bombers", pos: "MID", price: 223400, be: -49, status: "IN", own: "8.1%", vestRisk: "MED" },
-    { name: "Jai Serong", team: "Hawks", pos: "DEF", price: 276100, be: -37, status: "IN", own: "98.0%", vestRisk: "LOW" },
-    { name: "Max Gawn", team: "Demons", pos: "RUC", price: 703700, be: 185, status: "TEST", own: "44.2%", vestRisk: "ZERO" }
+    { name: "Angus Anderson", team: "Magpies", pos: "MID", price: 113500, be: -75, status: "IN", own: "45%", vestRisk: "LOW" },
+    { name: "Nasiah Wanganeen-Milera", team: "Saints", pos: "DEF", price: 588300, be: 94, status: "IN", own: "54%", vestRisk: "ZERO" },
+    { name: "Harry Sheezel", team: "Kangaroos", pos: "DEF", price: 645000, be: 112, status: "TEST", own: "70%", vestRisk: "ZERO" },
+    { name: "Nick Daicos", team: "Magpies", pos: "MID", price: 636200, be: 129, status: "IN", own: "35%", vestRisk: "ZERO" },
+    { name: "Isaac Heeney", team: "Swans", pos: "MID", price: 632700, be: 24, status: "OUT", own: "10%", vestRisk: "N/A" }
 ];
 
 const injuries = [
-    { name: "Isaac Heeney", team: "Swans", injury: "Hamstring", return: "1-2 weeks" },
+    { name: "Isaac Heeney", team: "Swans", injury: "Calf", return: "R7 OUT" },
+    { name: "Harry Sheezel", team: "Kangaroos", injury: "Finger", return: "TEST" },
     { name: "Mitch Hinge", team: "Crows", injury: "Hamstring", return: "2-4 weeks" },
     { name: "Jake Soligo", team: "Crows", injury: "Ankle", return: "TEST" },
+    { name: "Jarrod Berry", team: "Lions", injury: "Calf", return: "3-4 weeks" },
     { name: "Sam Banks", team: "Tigers", injury: "Concussion", return: "1 week" }
 ];
