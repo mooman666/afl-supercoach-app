c8b75161ba71e5a42f06720e3f8c0a8d63de42c2
mooman666 <ramartin@post.com>
2026-04-24 18:41:46 +1000

    Update players.js

--- players.js
+++ players.js
@@ -1,6 +1,5 @@
 const playerDatabase = [
     { name: "Angus Anderson", team: "Magpies", pos: "MID", price: 113500, be: -75, status: "IN", own: "45%" },
-    { name: "Nasiah Wanganeen-Milera", team: "Saints", pos: "DEF", price: 588300, be: 94, status: "IN", own: "54%" },
     { name: "Harry Sheezel", team: "Kangaroos", pos: "DEF", price: 645000, be: 112, status: "TEST", own: "70%" },
     { name: "Nick Daicos", team: "Magpies", pos: "MID", price: 636200, be: 129, status: "IN", own: "35%" },
     { name: "Isaac Heeney", team: "Swans", pos: "MID", price: 632700, be: 24, status: "OUT", own: "10%" }
