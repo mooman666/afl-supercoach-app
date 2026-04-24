312f82c129c44db1994b29f2dfe39700ca7b5f4b
mooman666 <ramartin@post.com>
2026-04-24 15:15:17 +1000

    Update players.js

--- players.js
+++ players.js
@@ -1,12 +1,15 @@
 const playerDatabase = [
-    { name: "Max Gawn", team: "Demons", pos: "RUC", price: 650000, avg: 134.2, bye: 14 },
-    { name: "Brodie Grundy", team: "Swans", pos: "RUC", price: 645000, avg: 133.0, bye: 12 },
-    { name: "Nick Daicos", team: "Magpies", pos: "MID", price: 710000, avg: 125.4, bye: 15 },
-    { name: "Harry Sheezel", team: "Kangaroos", pos: "DEF", price: 680000, avg: 128.0, bye: 14 },
-    { name: "Jordan Dawson", team: "Crows", pos: "MID", price: 620000, avg: 110.2, bye: 14 },
-    { name: "Isaac Heeney", team: "Swans", pos: "FWD", price: 590000, avg: 128.0, bye: 12 },
+    { name: "Max Gawn", team: "Demons", pos: "RUC", price: 703700, avg: 134.2, bye: 14 },
+    { name: "Brodie Grundy", team: "Swans", pos: "RUC", price: 698900, avg: 133.0, bye: 12 },
+    { name: "Tristan Xerri", team: "Kangaroos", pos: "RUC", price: 682500, avg: 105.0, bye: 14 },
+    { name: "Nick Daicos", team: "Magpies", pos: "MID", price: 636200, avg: 125.4, bye: 15 },
+    { name: "Harry Sheezel", team: "Kangaroos", pos: "MID", price: 611400, avg: 128.0, bye: 14 },
+    { name: "Isaac Heeney", team: "Swans", pos: "FWD", price: 666200, avg: 128.0, bye: 12 },
+    { name: "Zak Butters", team: "Power", pos: "MID", price: 653300, avg: 118.5, bye: 13 },
+    { name: "Jordan Dawson", team: "Crows", pos: "MID", price: 553100, avg: 110.2, bye: 14 },
+    { name: "Caleb Serong", team: "Dockers", pos: "MID", price: 541100, avg: 122.1, bye: 13 },
+    { name: "Sam Flanders", team: "Suns", pos: "FWD", price: 396600, avg: 115.5, bye: 12 },
     { name: "Hugh Bond", team: "Crows", pos: "DEF", price: 167800, avg: 83.0, bye: 14 },
-    { name: "Sam Flanders", team: "Suns", pos: "FWD", price: 580000, avg: 115.5, bye: 12 },
-    { name: "Tom Stewart", team: "Cats", pos: "DEF", price: 630000, avg: 108.9, bye: 15 },
-    { name: "Caleb Serong", team: "Dockers", pos: "MID", price: 690000, avg: 122.1, bye: 13 }
+    { name: "Jagga Smith", team: "Tigers", pos: "MID", price: 119900, avg: 0.0, bye: 12 },
+    { name: "Jai Serong", team: "Hawks", pos: "DEF", price: 119900, avg: 0.0, bye: 15 }
 ];
