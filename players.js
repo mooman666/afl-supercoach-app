c282fbab9e533f163cbf7a5d7369f4704751b30d
mooman666 <ramartin@post.com>
2026-04-24 15:42:56 +1000

    Update players.js

--- players.js
+++ players.js
@@ -1,27 +1,33 @@
 const playerDatabase = [
-    { name: "Max Gawn", team: "Demons", pos: "RUC", price: 703700, avg: 134.2, be: 142, cba: "85%", tog: "88%", bye: 14 },
-    { name: "Harry Sheezel", team: "Kangaroos", pos: "MID", price: 611400, avg: 128.0, be: 98, cba: "72%", tog: "92%", bye: 14 },
-    { name: "Sam Flanders", team: "Suns", pos: "FWD", price: 396600, avg: 115.5, be: 45, cba: "40%", tog: "82%", bye: 12 },
-    { name: "Nick Daicos", team: "Magpies", pos: "MID", price: 636200, avg: 125.4, be: 129, cba: "65%", tog: "90%", bye: 15 }
+    { name: "Max Gawn", team: "Demons", pos: "RUC", price: 703700, avg: 134.2, be: 142, cba: "88%", tog: "85%", bye: 14 },
+    { name: "Brodie Grundy", team: "Swans", pos: "RUC", price: 698900, avg: 133.0, be: 135, cba: "82%", tog: "80%", bye: 12 },
+    { name: "Tristan Xerri", team: "Kangaroos", pos: "RUC", price: 682500, avg: 115.0, be: 110, cba: "90%", tog: "82%", bye: 14 },
+    { name: "Nick Daicos", team: "Magpies", pos: "MID", price: 636200, avg: 125.4, be: 129, cba: "65%", tog: "90%", bye: 15 },
+    { name: "Harry Sheezel", team: "Kangaroos", pos: "MID/DEF", price: 611400, avg: 128.0, be: 98, cba: "20%", tog: "92%", bye: 14 },
+    { name: "Isaac Heeney", team: "Swans", pos: "MID/FWD", price: 666200, avg: 121.0, be: 145, cba: "75%", tog: "85%", bye: 12 },
+    { name: "Zak Butters", team: "Power", pos: "MID", price: 653300, avg: 118.5, be: 110, cba: "80%", tog: "82%", bye: 13 },
+    { name: "Marcus Bontempelli", team: "Dogs", pos: "MID", price: 720100, avg: 122.3, be: 130, cba: "85%", tog: "84%", bye: 15 },
+    { name: "Sam Flanders", team: "Suns", pos: "FWD", price: 396600, avg: 115.5, be: 45, cba: "45%", tog: "82%", bye: 12 },
+    { name: "Caleb Serong", team: "Dockers", pos: "MID", price: 641100, avg: 122.1, be: 115, cba: "82%", tog: "83%", bye: 13 },
+    { name: "Tom Stewart", team: "Cats", pos: "DEF", price: 605200, avg: 108.9, be: 112, cba: "5%", tog: "94%", bye: 15 },
+    { name: "Jordan Dawson", team: "Crows", pos: "MID", price: 583100, avg: 110.2, be: 105, cba: "70%", tog: "88%", bye: 14 },
+    { name: "Hugh Bond", team: "Crows", pos: "DEF", price: 167800, avg: 83.0, be: -12, cba: "0%", tog: "80%", bye: 14 },
+    { name: "Jagga Smith", team: "Tigers", pos: "MID", price: 119900, avg: 0, be: 25, cba: "0%", tog: "0%", bye: 12 },
+    { name: "Jai Serong", team: "Hawks", pos: "DEF", price: 119900, avg: 0, be: 25, cba: "0%", tog: "0%", bye: 15 }
 ];
 
 const newsUpdates = [
-    "🚨 LATE MAIL: Max Gawn training solo today.",
-    "📈 VALUE ALERT: Flanders set for $50k+ price jump."
+    "🚨 LATE MAIL: Max Gawn training solo today. Monitor backup rucks.",
+    "📈 MARKET WATCH: Flanders set for massive price rise if he hits 80+.",
+    "🩹 INJURY: Tom Stewart (Cats) subbed out with hammy tightness."
 ];
 
-// YOUR ARTICLES GO HERE
 const articles = [
     {
-        title: "The Mid-Season Cash Cow Strategy",
+        title: "The Mid-Season Pivot",
         date: "April 24, 2026",
         category: "Strategy",
-        content: "With the bye rounds approaching, the focus shifts to rookies with low Breakevens. Look at players like Jagga Smith who provide security..."
-    },
-    {
-        title: "Why CBA% is the Secret to Winning",
-        date: "April 22, 2026",
-        category: "Data Insights",
-        content: "Don't just look at the score. If a player's Center Bounce Attendance (CBA) jumps from 20% to 60%, a massive score is coming."
+        content: "As we hit Round 7, the focus shifts to clearing out failed mid-pricers for reliable premiums..."
     }
 ];
+
