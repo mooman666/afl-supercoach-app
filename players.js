718b1433f34cd5e86cc5d55f6ef5be9556cb8d98
mooman666 <ramartin@post.com>
2026-04-24 18:36:52 +1000

    Update players.js

--- players.js
+++ players.js
@@ -1,14 +1,12 @@
 const playerDatabase = [
-    { name: "Angus Anderson", team: "Magpies", pos: "MID", price: 113500, be: -75, status: "IN", own: "45%", vestRisk: "LOW" },
-    { name: "Nasiah Wanganeen-Milera", team: "Saints", pos: "DEF", price: 588300, be: 94, status: "IN", own: "54%", vestRisk: "ZERO" },
-    { name: "Harry Sheezel", team: "Kangaroos", pos: "DEF", price: 645000, be: 112, status: "TEST", own: "70%", vestRisk: "ZERO" },
-    { name: "Nick Daicos", team: "Magpies", pos: "MID", price: 636200, be: 129, status: "IN", own: "35%", vestRisk: "ZERO" },
-    { name: "Isaac Heeney", team: "Swans", pos: "MID", price: 632700, be: 24, status: "OUT", own: "10%", vestRisk: "N/A" }
+    { name: "Angus Anderson", team: "Magpies", pos: "MID", price: 113500, be: -75, status: "IN", own: "45%" },
+    { name: "Nasiah Wanganeen-Milera", team: "Saints", pos: "DEF", price: 588300, be: 94, status: "IN", own: "54%" },
+    { name: "Harry Sheezel", team: "Kangaroos", pos: "DEF", price: 645000, be: 112, status: "TEST", own: "70%" },
+    { name: "Nick Daicos", team: "Magpies", pos: "MID", price: 636200, be: 129, status: "IN", own: "35%" }
 ];
 
 const injuries = [
-    { name: "Isaac Heeney", team: "Swans", injury: "Calf Tightness", return: "R7 OUT" },
-    { name: "Harry Sheezel", team: "Kangaroos", injury: "Finger (Glove)", return: "PROBABLE" },
-    { name: "Jarrod Berry", team: "Lions", injury: "Calf", return: "3-4 weeks" },
+    { name: "Isaac Heeney", team: "Swans", injury: "Calf", return: "R7 OUT" },
+    { name: "Harry Sheezel", team: "Kangaroos", injury: "Finger", return: "TEST" },
     { name: "Mitch Hinge", team: "Crows", injury: "Hamstring", return: "2-4 weeks" }
 ];
