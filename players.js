4f7020f5fee547980eee9ce9513355bb1adb41d3
mooman666 <ramartin@post.com>
2026-04-24 18:51:22 +1000

    Update players.js

--- players.js
+++ players.js
@@ -1,11 +1,107 @@
+// 🟢 PLAYER DATABASE - Fixes the "undefined" status tags
 const playerDatabase = [
-    { name: "Angus Anderson", team: "Magpies", pos: "MID", price: 113500, be: -75, status: "IN", own: "45%" },
-    { name: "Harry Sheezel", team: "Kangaroos", pos: "DEF", price: 645000, be: 112, status: "TEST", own: "70%" },
-    { name: "Isaac Heeney", team: "Swans", pos: "MID", price: 632700, be: 24, status: "OUT", own: "10%" }
+    { 
+        name: "Angus Anderson", 
+        team: "Magpies", 
+        pos: "MID", 
+        price: 113500, 
+        be: -75, 
+        status: "IN", 
+        own: "45.2%", 
+        vestRisk: "LOW" 
+    },
+    { 
+        name: "Nasiah Wanganeen-Milera", 
+        team: "Saints", 
+        pos: "DEF", 
+        price: 588300, 
+        be: 94, 
+        status: "IN", 
+        own: "54.1%", 
+        vestRisk: "ZERO" 
+    },
+    { 
+        name: "Harry Sheezel", 
+        team: "Kangaroos", 
+        pos: "DEF", 
+        price: 645000, 
+        be: 112, 
+        status: "TEST", 
+        own: "70.4%", 
+        vestRisk: "ZERO" 
+    },
+    { 
+        name: "Nick Daicos", 
+        team: "Magpies", 
+        pos: "MID", 
+        price: 636200, 
+        be: 129, 
+        status: "IN", 
+        own: "35.8%", 
+        vestRisk: "ZERO" 
+    },
+    { 
+        name: "Isaac Heeney", 
+        team: "Swans", 
+        pos: "MID", 
+        price: 632700, 
+        be: 24, 
+        status: "OUT", 
+        own: "10.2%", 
+        vestRisk: "N/A" 
+    },
+    { 
+        name: "Jai Serong", 
+        team: "Hawks", 
+        pos: "DEF", 
+        price: 276100, 
+        be: 42, 
+        status: "IN", 
+        own: "98.0%", 
+        vestRisk: "LOW" 
+    },
+    { 
+        name: "Elijah Tsatas", 
+        team: "Bombers", 
+        pos: "MID", 
+        price: 223400, 
+        be: -49, 
+        status: "IN", 
+        own: "8.1%", 
+        vestRisk: "MED" 
+    }
 ];
 
+// 🔴 INJURY LIST - Fixes the blank "Injury Intelligence" page
 const injuries = [
-    { name: "Isaac Heeney", team: "Swans", injury: "Calf", return: "R7 OUT" },
-    { name: "Harry Sheezel", team: "Kangaroos", injury: "Finger", return: "TEST" },
-    { name: "Jarrod Berry", team: "Lions", injury: "Calf", return: "3-4 Weeks" }
+    { 
+        name: "Isaac Heeney", 
+        team: "Swans", 
+        injury: "Calf Tightness", 
+        return: "R7 OUT" 
+    },
+    { 
+        name: "Harry Sheezel", 
+        team: "Kangaroos", 
+        injury: "Finger (Glove)", 
+        return: "PROBABLE" 
+    },
+    { 
+        name: "Mitch Hinge", 
+        team: "Crows", 
+        injury: "Hamstring", 
+        return: "2-4 Weeks" 
+    },
+    { 
+        name: "Jarrod Berry", 
+        team: "Lions", 
+        injury: "Calf", 
+        return: "3-4 Weeks" 
+    },
+    { 
+        name: "Sam Banks", 
+        team: "Tigers", 
+        injury: "Concussion", 
+        return: "1 Week" 
+    }
 ];
