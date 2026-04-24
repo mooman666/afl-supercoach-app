b398cbd1bad0dbdeffc125216867351b682ffcb6
mooman666 <ramartin@post.com>
2026-04-24 15:26:35 +1000

    Update players.js

--- players.js
+++ players.js
@@ -1,15 +1,17 @@
 const playerDatabase = [
-    { name: "Max Gawn", team: "Demons", pos: "RUC", price: 703700, avg: 134.2, bye: 14 },
-    { name: "Brodie Grundy", team: "Swans", pos: "RUC", price: 698900, avg: 133.0, bye: 12 },
-    { name: "Tristan Xerri", team: "Kangaroos", pos: "RUC", price: 682500, avg: 105.0, bye: 14 },
-    { name: "Nick Daicos", team: "Magpies", pos: "MID", price: 636200, avg: 125.4, bye: 15 },
-    { name: "Harry Sheezel", team: "Kangaroos", pos: "MID", price: 611400, avg: 128.0, bye: 14 },
-    { name: "Isaac Heeney", team: "Swans", pos: "FWD", price: 666200, avg: 128.0, bye: 12 },
-    { name: "Zak Butters", team: "Power", pos: "MID", price: 653300, avg: 118.5, bye: 13 },
-    { name: "Jordan Dawson", team: "Crows", pos: "MID", price: 553100, avg: 110.2, bye: 14 },
-    { name: "Caleb Serong", team: "Dockers", pos: "MID", price: 541100, avg: 122.1, bye: 13 },
-    { name: "Sam Flanders", team: "Suns", pos: "FWD", price: 396600, avg: 115.5, bye: 12 },
-    { name: "Hugh Bond", team: "Crows", pos: "DEF", price: 167800, avg: 83.0, bye: 14 },
-    { name: "Jagga Smith", team: "Tigers", pos: "MID", price: 119900, avg: 0.0, bye: 12 },
-    { name: "Jai Serong", team: "Hawks", pos: "DEF", price: 119900, avg: 0.0, bye: 15 }
+    { name: "Max Gawn", team: "Demons", pos: "RUC", price: 703700, avg: 134.2, last3: 138.5, be: 142, bye: 14 },
+    { name: "Brodie Grundy", team: "Swans", pos: "RUC", price: 698900, avg: 133.0, last3: 131.0, be: 135, bye: 12 },
+    { name: "Nick Daicos", team: "Magpies", pos: "MID", price: 636200, avg: 125.4, last3: 118.0, be: 129, bye: 15 },
+    { name: "Harry Sheezel", team: "Kangaroos", pos: "MID", price: 611400, avg: 128.0, last3: 142.2, be: 98, bye: 14 },
+    { name: "Isaac Heeney", team: "Swans", pos: "FWD", price: 666200, avg: 128.0, last3: 115.4, be: 145, bye: 12 },
+    { name: "Zak Butters", team: "Power", pos: "MID", price: 653300, avg: 118.5, last3: 121.0, be: 110, bye: 13 },
+    { name: "Sam Flanders", team: "Suns", pos: "FWD", price: 396600, avg: 115.5, last3: 119.0, be: 45, bye: 12 },
+    { name: "Hugh Bond", team: "Crows", pos: "DEF", price: 167800, avg: 83.0, last3: 83.0, be: -12, bye: 14 },
+    { name: "Jagga Smith", team: "Tigers", pos: "MID", price: 119900, avg: 0, last3: 0, be: 25, bye: 12 }
+];
+
+const newsUpdates = [
+    { date: "April 24", text: "🚨 LATE MAIL: Max Gawn (Demons) training solo. Monitor Ruck backup." },
+    { date: "April 23", text: "📈 MARKET WATCH: Sam Flanders set for $50k+ rise if he clears 80pts." },
+    { date: "April 22", text: "🩹 INJURY ROOM: Tom Stewart (Cats) hammy tightness. Test required." }
 ];
